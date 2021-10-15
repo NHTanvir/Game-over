aa9225370f076baadb35746e8809ab6d5a33c3e6
NHTanvir <36070006+NHTanvir@users.noreply.github.com>
2021-10-15 15:55:49 +0600

    index.php
    All three Problem solved

--- index.php
+++ index.php
@@ -11,42 +11,6 @@
 <h1>Palindrome</h1>
 <?php
 
-// $inputt = "rat";
-// //custom rev
-// $arr = str_split($inputt);
-
-// $index = count($arr);
-// $i =0;
-// while($i < $index) {
-
-//     $a = $arr[--$index];
-//         // if( $i ==$index && $val == $inputt){ 
-//         //     echo "Palindrome"; 
-//         //     break;
-//         //         }
-//         // else { 
-//         //     echo "Not a Palindrome"; 
-//         //     break;
-//         // }
-        
-//             }
-// echo $a;
-// //another
-// for($x=strlen($inputt)-1; $x>=0; $x--)
-// {
-//    $a= $inputt[$x];
-//    if( $val == $inputt){ 
-//     echo "Palindrome"; 
-//     break;
-//         }
-//     else { 
-//     echo "Not a Palindrome"; 
-//     break;
-// }
-// }
-//anayway
-// PHP code to check for Palindrome string in PHP
-// Using strrev()
 $test = "DAD";
 function Palindrome($string){ 
     if (strrev($string) == $string){ 
@@ -72,10 +36,8 @@ echo "Not a Palindrome";
 function done($main){
 
     $rev = strrev($main);
-    //$array1 = str_split($main);
     $counted =strlen($main);
     $half =   round($counted /2) -2;  
-
     $orginal= substr($rev,$half+2);
     $first = strrev($orginal);
     $last = substr($main,$half+2);
@@ -86,41 +48,48 @@ function done($main){
         }
 }
 
-done('abzaba');
+done('abzab');
 
 ?>
 <h1>Third</h1>
 <?php
 
-//without loop first half
-function third($arrey){
-    rsort($arrey); 
-    $ssum = $arrey[0]+$arrey[1];
-    echo $ssum;
-}
-third(array(1,2,3,4,5,6,7,8,1,9,14,18));
-
-//with loop
-
-$arr = array(4, 6, 8, 10, 12, 14, 16); 
-$counted = count($arr);
-$new =[];
-for($i=0;$i<$counted;$i++)
-    {
-    
-    for($j=$i+1;$j<$counted;$j++)
-{    array_push($new, $arr[$i] + $arr[$j]);
-     echo $arr[$i] + $arr[$j];
-    
-    
+function third($arr){
+     
+    $counted = count($arr);
+    $new =[];
+    for($i=0;$i<$counted;$i++){
+        for($j=$i+1;$j<$counted;$j++){
+            array_push($new, $arr[$i] + $arr[$j]);
+        
+    }
+    }
+    //print_r($new);
+    echo 'The max sum of every Two Number is number is : '.max($new);
+    $final_array=[];
     ?>
     <br>
     <?php
-}
-}
-print_r($new);
+    //spliting evey value than merge into one array
+    foreach ($new as $value) {
+        array_push($final_array,str_split($value));
+    }
+    $ultimate =call_user_func_array('array_merge', $final_array);
+    ?>
+    <br>
+    <?php
+    $final_count = count($ultimate);
+    $result_array=[];
+    for($i=0,$j=1;$i,$j<$final_count;$i+=2,$j+=2){ 
+        array_push($result_array, $ultimate[$i] + $ultimate[$j]);
+    }
+    echo 'The max sum of all previous result is : '.max($result_array);
+    }
+third(array(4, 6, 8, 10, 12, 14));
+    
 
 ?>
+
 
 
 </body>
